0eea016550b26da3906293aca6180675708e1d0b
allan-cedric <allan.py3000@gmail.com>
2025-12-30 05:41:30 -0300

    feat: Add parameter examples for task project

--- FreeRTOS/task/main/main.c
+++ FreeRTOS/task/main/main.c
@@ -11,12 +11,33 @@ static const char *TAG1 = "TASK APP MAIN: ";
 static const char *TAG2 = "TASK BLINK: ";
 static const char *TAG3 = "TASK COUNTER: ";
 
-#define LED_PIN 14
+#define LED1_PIN 14
+#define LED2_PIN 21
+#define BLINK1_PERIOD 200
+#define BLINK2_PERIOD 500
+
+typedef struct 
+{
+    int led_pin;
+    int blink_period;
+}led_t;
+
+led_t led1 = {
+    LED1_PIN, BLINK1_PERIOD
+};
+
+led_t led2 = {
+    LED2_PIN, BLINK2_PERIOD
+};
+
 
 void vTaskBlink(void *pvParameters);
 void vTaskCounter(void *pvParameters);
-TaskHandle_t xTaskBlinkHandle = NULL;
+TaskHandle_t xTaskBlink1Handle = NULL;
+TaskHandle_t xTaskBlink2Handle = NULL;
 TaskHandle_t xTaskCounterHandle = NULL;
+
+int valor = 123;
 
 void app_main(void)
 {
@@ -24,18 +45,27 @@ void app_main(void)
 
     xTaskCreate(
         vTaskBlink, // funcao da task
-        "TaskBlink",
+        "TaskBlink1",
         2048,
-        NULL,
+        (void *)&led1,
         1,
-        &xTaskBlinkHandle
+        &xTaskBlink1Handle
+    );
+
+    xTaskCreate(
+        vTaskBlink, // funcao da task
+        "TaskBlink2",
+        2048,
+        (void *)&led2,
+        1,
+        &xTaskBlink2Handle
     );
 
     xTaskCreate(
         vTaskCounter, // funcao da task
         "TaskCounter",
         2048,
-        NULL,
+        (void *)valor,
         2,
         &xTaskCounterHandle
     );
@@ -51,22 +81,24 @@ void app_main(void)
 
 void vTaskBlink(void *pvParameters)
 {
+    int led_pin = ((led_t*)pvParameters)->led_pin;
+    int blink_period = ((led_t*)pvParameters)->blink_period;
     bool status = false;
 
     ESP_LOGI(TAG2, "Iniciando a task Blink");
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
 
     while(1)
     {
-        gpio_set_level(LED_PIN, status ^= 1);
-        ESP_LOGI(TAG2, "TASK Blink LED[%d]: %d", LED_PIN, status);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        gpio_set_level(led_pin, status ^= 1);
+        ESP_LOGI(TAG2, "[%s] LED[%d]: %d", pcTaskGetName(NULL),led_pin, status);
+        vTaskDelay(pdMS_TO_TICKS(blink_period));
     }
 }
 
 void vTaskCounter(void *pvParameters)
 {
-    int i = 1000;
+    int i = (int)pvParameters;
 
     ESP_LOGI(TAG3, "Iniciando a task Counter");
 
@@ -74,31 +106,31 @@ void vTaskCounter(void *pvParameters)
     {
         ESP_LOGI(TAG3, "Counter: %d", i++);
 
-        if(i == 1010)
-        {
-            // vTaskDelete(xTaskBlinkHandle);
-            vTaskSuspend(xTaskBlinkHandle);
-            gpio_set_level(LED_PIN, 0);
-            ESP_LOGI(TAG3, "TASK Blink suspensa");
-        }
-        else if (i == 1015)
-        {
-            // xTaskCreate(
-            //     vTaskBlink, // funcao da task
-            //     "TaskBlink",
-            //     2048,
-            //     NULL,
-            //     1,
-            //     &xTaskBlinkHandle
-            // );
+        // if(i == 1010)
+        // {
+        //     // vTaskDelete(xTaskBlinkHandle);
+        //     vTaskSuspend(xTaskBlinkHandle);
+        //     gpio_set_level(LED_PIN, 0);
+        //     ESP_LOGI(TAG3, "TASK Blink suspensa");
+        // }
+        // else if (i == 1015)
+        // {
+        //     // xTaskCreate(
+        //     //     vTaskBlink, // funcao da task
+        //     //     "TaskBlink",
+        //     //     2048,
+        //     //     NULL,
+        //     //     1,
+        //     //     &xTaskBlinkHandle
+        //     // );
 
-            // i = 1000;
-            // vTaskDelete(NULL);
+        //     // i = 1000;
+        //     // vTaskDelete(NULL);
 
-            vTaskResume(xTaskBlinkHandle);
-            i = 1000;
-            ESP_LOGI(TAG3, "TASK Blink resumed");
-        }
+        //     vTaskResume(xTaskBlinkHandle);
+        //     i = 1000;
+        //     ESP_LOGI(TAG3, "TASK Blink resumed");
+        // }
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
